fd28c31d1ceba1c08410b3a331e465e879716cbd
Olivier <ducrouxolivier@gmail.com>
2025-05-21 13:54:40 +0200

    setup deployment

---
+++ db/production.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS theme_astral_production;
+CREATE DATABASE IF NOT EXISTS theme_astral_production_cache;
+CREATE DATABASE IF NOT EXISTS theme_astral_production_queue;
+CREATE DATABASE IF NOT EXISTS theme_astral_production_cable;
