0b36d060b7b7bbe43762eb50eea9c4a4f44293c8
Olivier <ducrouxolivier@gmail.com>
2025-06-08 00:29:54 +0200

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def new_contact_notification(contact)
     @contact = contact
     mail(
-      to: "ducrouxolivier@gmail.com", # Replace with your email
+      to: ["ducrouxolivier@gmail.com", "arianejonker.fr@gmail.com"], # Replace with your email
       subject: "New Contact Form Submission from #{contact.name}"
     )
   end
